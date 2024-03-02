54e97fcb21070b5c30ec0b31bf1560f115ef4ea8
harshvardhan mamgai <harshitmamgai11@gmail.com>
2024-03-03 00:12:02 +0530

    added message and conversation model

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,22 @@
+import mongoose, { MongooseError } from 'mongoose';
+
+const conversationSchema= mongoose.Schema({
+participants:[
+    {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+},
+],
+messages:[
+    {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Message',
+    default:[],
+},
+],
+
+},{timestamps:true});
+
+const Conversation= new mongoose.model('Conversation',conversationSchema);
+
+export default Conversation;
