41a68a6a2e3281500a1f12aaac172f9bda99098f
harshvardhan mamgai <harshitmamgai11@gmail.com>
2024-03-03 23:34:20 +0530

    connected frontend with backend and also setup signup functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import{Routes,Route}  from 'react-router-dom'
 import Home from  "./pages/home/Home"
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/Signup";
+import {Toaster} from 'react-hot-toast';
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
 				<Route path="/login" element={<Login />}/>
 				<Route path="/signup" element={<SignUp/>}/>
 			</Routes>
-		
+		<Toaster/>
 		</div>
 	);
 }
