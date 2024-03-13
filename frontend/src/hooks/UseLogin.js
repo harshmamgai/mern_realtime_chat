095335e38502fd12c5ea9902cb740ef5b5b6c722
harshvardhan mamgai <harshitmamgai11@gmail.com>
2024-03-14 01:06:35 +0530

    loginFunctionality done

---
+++ frontend/src/hooks/UseLogin.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { useAuthContext } from "../context/AuthContext";
+import toast from "react-hot-toast";
+
+
+
+const UseLogin = () => {
+const {setAuthUser}= useAuthContext()
+const [loading,setLoading]=useState(false);
+const login=async(username,password)=>{
+    setLoading(true)
+  try{
+const res= await fetch("/api/auth/login",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({username,password})
+})
+const data= await res.json()
+if(data.error){
+  console.log(data.error)
+    throw new Error(data.error)
+    
+}
+localStorage.setItem("chat-user",JSON.stringify(data))
+setAuthUser(data);
+  }
+  catch(error){
+toast.error(error.message)
+  }
+  finally{
+    setLoading(false);
+  }
+
+}
+return {loading,login};
+}
+export default UseLogin;
