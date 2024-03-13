9b203c0c628312501704af4df3acad707e0f655d
harshvardhan mamgai <harshitmamgai11@gmail.com>
2024-03-14 00:09:03 +0530

    added protected routes using the context

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -4,7 +4,7 @@ import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
 	const [loading, setLoading] = useState(false);
-	const[authUser,setAuthUser]=useAuthContext();
+	const{setAuthUser}=useAuthContext();
 
 	const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
 		const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
@@ -20,9 +20,14 @@ const useSignup = () => {
 
 			const data = await res.json();
 			console.log(data);
-			
+			toast.success("user created successfully");
+
+
+if(data.error){
+	throw   new Error(data.error);
+}
 //localstorage
-localStorage.setItem("chat-user",JSON.stringify(data));
+localStorage.setItem("chat-user",JSON.stringify(data))
 //context
 setAuthUser(data);
 		} 
