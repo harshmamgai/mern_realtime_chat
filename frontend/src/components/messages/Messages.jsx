51431628107a9272273c3f92bb14a6b85af57362
harshvardhan mamgai <harshitmamgai11@gmail.com>
2024-03-14 20:37:31 +0530

    messages showing correctly in screen

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,21 +1,29 @@
 import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
+MessageSkeleton
 const Messages = () => {
 
   const{loading,messages}=useGetMessages();
-  console.log(messages);
+  //as soon as we send a message automatically the message should 
  
  return (
     <div className="px-4 flex-1 overflow-auto">
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+
+{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} >
+						<Message message={message} />
+					</div>
+				))}
+
+    {/* to  show when data is loading */}
+       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+       {/* to show when there is no conversation b/w users */}
+       {!loading && messages.length === 0 && (
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
     </div>
   )
 	
