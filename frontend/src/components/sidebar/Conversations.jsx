c62310a948d24f1be2398df393aa5f752704ba73
harshvardhan mamgai <harshitmamgai11@gmail.com>
2024-03-14 12:42:07 +0530

    fetched users in sidebar and fixing data fetch issue

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,22 @@
 
+import useGetConversations from "../../hooks/useGetConversation";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
 	return (
 		<div className='py-2 flex flex-col overflow-auto'>
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
+			{conversations.filterdUsers.map((conversation, idx) => (
+                <Conversation
+                    key={conversation._id}
+                    conversation={conversation}
+                    emoji={getRandomEmoji()}
+                    lastIdx={idx === conversations.filterdUsers.length - 1}
+                />
+            ))}
+
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
 		</div>
 	);
 };
